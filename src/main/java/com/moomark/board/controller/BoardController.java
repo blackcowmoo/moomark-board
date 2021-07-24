338961e5ea23e99c01bbdd1778fa163223576c6a
Apisapple <38449976+Apisapple@users.noreply.github.com>
2021-07-25 00:43:27 +0900

    Update BoardController.java
    board 주석 추가

--- src/main/java/com/moomark/board/controller/BoardController.java
+++ src/main/java/com/moomark/board/controller/BoardController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.moomark.board.domain.BoardDto;
@@ -19,16 +18,19 @@ public class BoardController {
 	private final BoardService boardService;
 	
 	
+	/*Get*/
 	@GetMapping("/board/{boardId}/content")
 	public BoardDto getBoardInfoById(@PathVariable("boardId") Long boardId) {
 		return boardService.getBoardInfoById(boardId);
 	}
 	
+	/*Post*/
 	@PostMapping("/board/new")
 	public Long saveBoardInfo(BoardDto boardDto) {
 		return boardService.saveBoard(boardDto);
 	}
 	
+	/*Delete*/
 	@DeleteMapping("/board/{boardId}")
 	public void deleteBoardInfoById(@PathVariable("boardId") Long boardId) {
 		boardService.deleteBoard(boardId);
