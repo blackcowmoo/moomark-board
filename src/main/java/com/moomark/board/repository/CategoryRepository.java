764d3c753e4261376f9b17817e97115461ca2084
Apisapple <38449976+Apisapple@users.noreply.github.com>
2021-07-27 00:53:07 +0900

    Update CategoryRepository.java
    CategoryType을 가지고 찾는 함수 추가

--- src/main/java/com/moomark/board/repository/CategoryRepository.java
+++ src/main/java/com/moomark/board/repository/CategoryRepository.java
@@ -1,9 +1,11 @@
 package com.moomark.board.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.moomark.board.domain.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long>{
-
+	public Optional<Category> findByCategoryType(String categoryType);
 }
