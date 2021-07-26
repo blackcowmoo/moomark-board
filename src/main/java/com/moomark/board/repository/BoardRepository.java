52531a6b81655b6aac9c764519c3fa4b6ab8677e
Apisapple <38449976+Apisapple@users.noreply.github.com>
2021-07-27 00:52:02 +0900

    Update BoardRepository.java
    BoardCategory로 찾는 함수 추가

--- src/main/java/com/moomark/board/repository/BoardRepository.java
+++ src/main/java/com/moomark/board/repository/BoardRepository.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.moomark.board.domain.Board;
+import com.moomark.board.domain.BoardCategory;
 
 public interface BoardRepository extends JpaRepository<Board, Long>{
 	public List<Board> findByTitle(String title);
 	public List<Board> findByAuthorId(Long authorId);
+	public List<Board> findByBoardCategory(List<BoardCategory> boardCategory);
 }
