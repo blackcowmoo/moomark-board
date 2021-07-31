6f9017fc24624abf249299be7ed034361da0e79c
Apisapple <38449976+Apisapple@users.noreply.github.com>
2021-08-01 01:52:30 +0900

    Create CategoryController.java
    Category Controller 추가

---
+++ src/main/java/com/moomark/board/controller/CategoryController.java
@@ -0,0 +1,47 @@
+package com.moomark.board.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.moomark.board.service.CategoryService;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class CategoryController {
+	private final CategoryService categoryService;
+	
+	/*static class*/
+	@Data
+	static class RequestCategoryInfo {
+		Long categoryId;
+		String categoryInfo;
+	}
+	
+	/* Get */
+	
+	/* Post */
+	@PostMapping("/category/{info}")
+	public Long addCategoryInfo(@PathVariable("info") String cateogryInfo) {
+		return categoryService.addCategory(cateogryInfo);
+	}
+	
+	/* Put */
+	@PutMapping("/category/child")
+	public void updateCategoryInfo(@RequestBody RequestCategoryInfo requestCategoryInfo) throws Exception {
+		categoryService.updateCategory(requestCategoryInfo.getCategoryId(), 
+				requestCategoryInfo.getCategoryInfo());
+	}
+	
+	/* Delete */
+	@DeleteMapping("/category")
+	public void delteCateogry(@RequestBody RequestCategoryInfo requestCategoryInfo) throws Exception {
+		categoryService.deleteCategory(requestCategoryInfo.getCategoryId());
+	}
+}
