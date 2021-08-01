fb8cb7638b8bd7982046e2c99ae85a47259363fd
Apisapple <38449976+Apisapple@users.noreply.github.com>
2021-08-01 17:55:38 +0900

    Update CategoryController.java
    카테고리 정보 조회 함수 추가

--- src/main/java/com/moomark/board/controller/CategoryController.java
+++ src/main/java/com/moomark/board/controller/CategoryController.java
@@ -1,12 +1,14 @@
 package com.moomark.board.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.moomark.board.domain.CategoryDto;
 import com.moomark.board.service.CategoryService;
 
 import lombok.Data;
@@ -25,6 +27,10 @@ public class CategoryController {
 	}
 	
 	/* Get */
+	@GetMapping("/category/{id}")
+	public CategoryDto getCategoryInfo(@PathVariable("id") Long categoryId) throws Exception {
+		return categoryService.getCategoryById(categoryId);
+	}
 	
 	/* Post */
 	@PostMapping("/category/{info}")
