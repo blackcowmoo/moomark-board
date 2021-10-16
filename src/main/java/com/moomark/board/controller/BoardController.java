52d7d713e848b5f5349b9055beb59ebc29f365ca
Apisapple <38449976+Apisapple@users.noreply.github.com>
2021-10-17 02:49:56 +0900

    update code style
    google code style 적용

--- src/main/java/com/moomark/board/controller/BoardController.java
+++ src/main/java/com/moomark/board/controller/BoardController.java
@@ -19,42 +19,49 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 public class BoardController {
-	
-	private final BoardService boardService;
-	private final CommentService commentService;
-	
-	/*=================================== GET ===================================*/
-	@GetMapping("/board/{boardId}/content")
-	public BoardDto getBoardInfoById(@PathVariable("boardId") Long boardId) {
-		return boardService.getBoardInfoById(boardId);
-	}
-	
-	@GetMapping("/board/{boardId}/info")
-	public RequestTotalBoardInfo getTotalBoardInfoById(@PathVariable("boardId") Long boardId) throws Exception {
-		RequestTotalBoardInfo result = new RequestTotalBoardInfo();
-		result.setBoardInfo(boardService.getBoardInfoById(boardId));
-		result.setTotalCommentCount(commentService.getCommentCountByBoardId(boardId));
-		result.setCommentList(commentService.getCommentByBoardId(boardId));
-		
-		return result;
-	}
-	
-	/*=================================== POST ==================================*/
-	@PostMapping("/board/new")
-	public Long saveBoardInfo(BoardDto boardDto) {
-		return boardService.saveBoard(boardDto);
-	}
-	
-	@DeleteMapping("/board/{boardId}")
-	public void deleteBoardInfoById(@PathVariable("boardId") Long boardId) {
-		boardService.deleteBoard(boardId);
-	}
-	
-	/*============================= Static class ================================*/
-	@Data
-	private class RequestTotalBoardInfo {
-		BoardDto boardInfo;
-		int totalCommentCount;
-		List<CommentDto> commentList;
-	}
+
+  private final BoardService boardService;
+  private final CommentService commentService;
+
+  /*
+   * =================================== GET ===================================
+   */
+  @GetMapping("/board/{boardId}/content")
+  public BoardDto getBoardInfoById(@PathVariable("boardId") Long boardId) {
+    return boardService.getBoardInfoById(boardId);
+  }
+
+  @GetMapping("/board/{boardId}/info")
+  public RequestTotalBoardInfo getTotalBoardInfoById(@PathVariable("boardId") Long boardId)
+      throws Exception {
+    RequestTotalBoardInfo result = new RequestTotalBoardInfo();
+    result.setBoardInfo(boardService.getBoardInfoById(boardId));
+    result.setTotalCommentCount(commentService.getCommentCountByBoardId(boardId));
+    result.setCommentList(commentService.getCommentByBoardId(boardId));
+
+    return result;
+  }
+
+  /*
+   * =================================== POST ==================================
+   */
+  @PostMapping("/board/new")
+  public Long saveBoardInfo(BoardDto boardDto) {
+    return boardService.saveBoard(boardDto);
+  }
+
+  @DeleteMapping("/board/{boardId}")
+  public void deleteBoardInfoById(@PathVariable("boardId") Long boardId) {
+    boardService.deleteBoard(boardId);
+  }
+
+  /*
+   * ============================= Static class ================================
+   */
+  @Data
+  private class RequestTotalBoardInfo {
+    BoardDto boardInfo;
+    int totalCommentCount;
+    List<CommentDto> commentList;
+  }
 }
