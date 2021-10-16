fa191c36217ff83b149f9bcad6c2fb9deaf13954
Apisapple <38449976+Apisapple@users.noreply.github.com>
2021-10-17 03:15:29 +0900

    add board and category mapping
    게시판과 카테고리 mapping 추가

--- src/main/java/com/moomark/board/controller/CategoryController.java
+++ src/main/java/com/moomark/board/controller/CategoryController.java
@@ -17,54 +17,69 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 public class CategoryController {
-	private final CategoryService categoryService;
-	
-	/*static class*/
-	@Data
-	static class RequestCategoryInfo {
-		Long categoryId;
-		String categoryInfo;
-	}
-	
-	@Data
-	static class RequestChildCategory {
-		Long parentId;
-		Long childId;
-	}
-	
-	/* Get */
-	@GetMapping("/category/{id}")
-	public CategoryDto getCategoryInfo(@PathVariable("id") Long categoryId) throws Exception {
-		return categoryService.getCategoryById(categoryId);
-	}
-	
-	/* Post */
-	@PostMapping("/category/{info}")
-	public Long addCategoryInfo(@PathVariable("info") String cateogryInfo) {
-		return categoryService.addCategory(cateogryInfo);
-	}
-	
-	@PostMapping("/category/child")
-	public void addChildCategory(@RequestBody RequestChildCategory request) {
-		categoryService.addChildCategory(request.parentId, request.childId);
-	}
-	
-	/* Put */
-	@PutMapping("/category/child")
-	public void updateCategoryInfo(@RequestBody RequestCategoryInfo requestCategoryInfo) throws Exception {
-		categoryService.updateCategory(requestCategoryInfo.getCategoryId(), 
-				requestCategoryInfo.getCategoryInfo());
-	}
-	
-	/* Delete */
-	@DeleteMapping("/category")
-	public void deleteCateogory(@RequestBody RequestCategoryInfo requestCategoryInfo) throws Exception {
-		categoryService.deleteCategory(requestCategoryInfo.getCategoryId());
-	}
-	
-	@DeleteMapping("/category/child")
-	public void deleteChildCategory(@RequestBody RequestChildCategory requset) throws Exception {
-		categoryService.deleteChildCategory(requset.parentId, requset.childId);
-		
-	}
+  private final CategoryService categoryService;
+
+  /* static class */
+  @Data
+  static class RequestCategoryInfo {
+    Long categoryId;
+    String categoryInfo;
+  }
+
+  @Data
+  static class RequestChildCategory {
+    Long parentId;
+    Long childId;
+  }
+
+  @Data
+  static class RequestAddCategoryToBoard {
+    Long boardId;
+    Long categoryId;
+  }
+
+  /* Get */
+  @GetMapping("/category/{id}")
+  public CategoryDto getCategoryInfo(@PathVariable("id") Long categoryId) throws Exception {
+    return categoryService.getCategoryById(categoryId);
+  }
+
+  /* Post */
+  @PostMapping("/category/{info}")
+  public Long addCategoryInfo(@PathVariable("info") String cateogryInfo) {
+    return categoryService.addCategory(cateogryInfo);
+  }
+
+  @PostMapping("/category/child")
+  public void addChildCategory(@RequestBody RequestChildCategory request) {
+    categoryService.addChildCategory(request.parentId, request.childId);
+  }
+
+  @PostMapping("/category/mapping")
+  public void addCategoryToBoar(@RequestBody RequestAddCategoryToBoard requestInformation)
+      throws Exception {
+    categoryService.addCateogryToBoard(requestInformation.getBoardId(),
+        requestInformation.getCategoryId());
+  }
+
+  /* Put */
+  @PutMapping("/category/child")
+  public void updateCategoryInfo(@RequestBody RequestCategoryInfo requestCategoryInfo)
+      throws Exception {
+    categoryService.updateCategory(requestCategoryInfo.getCategoryId(),
+        requestCategoryInfo.getCategoryInfo());
+  }
+
+  /* Delete */
+  @DeleteMapping("/category")
+  public void deleteCateogory(@RequestBody RequestCategoryInfo requestCategoryInfo)
+      throws Exception {
+    categoryService.deleteCategory(requestCategoryInfo.getCategoryId());
+  }
+
+  @DeleteMapping("/category/child")
+  public void deleteChildCategory(@RequestBody RequestChildCategory requset) throws Exception {
+    categoryService.deleteChildCategory(requset.parentId, requset.childId);
+
+  }
 }
