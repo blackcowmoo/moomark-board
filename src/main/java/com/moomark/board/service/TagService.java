94bd851e53784f7e9a1fe0d475dec4dd8ef9e1a0
Apisapple <38449976+Apisapple@users.noreply.github.com>
2021-10-27 02:30:52 +0900

    Update TagService.java
    Error code 내용 추가

--- src/main/java/com/moomark/board/service/TagService.java
+++ src/main/java/com/moomark/board/service/TagService.java
@@ -5,6 +5,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.moomark.board.domain.Tag;
 import com.moomark.board.domain.TagDto;
+import com.moomark.board.exception.ErrorCode;
+import com.moomark.board.exception.JpaException;
 import com.moomark.board.repository.TagRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -16,26 +18,44 @@ import lombok.extern.slf4j.Slf4j;
 @Transactional(readOnly = true)
 public class TagService {
 
-	private final TagRepository tagRepository;
-	
-	public Long addTag(String information) {
-		var tag = Tag.builder()
-				.information(information)
-				.build();
-		
-		log.info("Add information : {}", information);
-		return	tagRepository.save(tag).getId();
-	}
-	
-	public void deleteTag(Long id) {
-		tagRepository.deleteById(id);
-	}
-	
-	public TagDto findTagById(Long id) {
-		var tag = tagRepository.findById(id).orElseThrow();
-		return TagDto.builder()
-				.id(tag.getId())
-				.information(tag.getInformation())
-				.build();
-	}
+  private final TagRepository tagRepository;
+
+  /**
+   * Add
+   * 
+   * @param information
+   * @return
+   */
+  public Long addTag(String information) {
+    var tag = Tag.builder().information(information).build();
+
+    log.info("Add information : {}", information);
+    return tagRepository.save(tag).getId();
+  }
+
+  /**
+   * Delete tag information
+   * 
+   * @param id
+   * @throws JpaException
+   */
+  public void deleteTag(Long id) throws JpaException {
+    var tag = tagRepository.findById(id)
+        .orElseThrow(() -> new JpaException(ErrorCode.CANNOT_FIND_TAG_INFORMATION.getMsg(),
+            ErrorCode.CANNOT_FIND_TAG_INFORMATION.getCode()));
+    tagRepository.deleteById(tag.getId());
+  }
+
+  /**
+   * find tag information by tag id
+   * @param id
+   * @return
+   * @throws JpaException
+   */
+  public TagDto findTagById(Long id) throws JpaException {
+    var tag = tagRepository.findById(id)
+        .orElseThrow(() -> new JpaException(ErrorCode.CANNOT_FIND_TAG_INFORMATION.getMsg(),
+            ErrorCode.CANNOT_FIND_TAG_INFORMATION.getCode()));
+    return TagDto.builder().id(tag.getId()).information(tag.getInformation()).build();
+  }
 }
