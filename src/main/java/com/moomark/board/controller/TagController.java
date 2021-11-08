4b3f1370ba6e1669976a46a1a3085b06ee1c775e
Apisapple <38449976+Apisapple@users.noreply.github.com>
2021-11-09 02:59:17 +0900

    Update TagController.java
    태그 컨트롤러 수정 작업 진행 중

--- src/main/java/com/moomark/board/controller/TagController.java
+++ src/main/java/com/moomark/board/controller/TagController.java
@@ -1,7 +1,12 @@
 package com.moomark.board.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.moomark.board.service.TagService;
+import com.moomark.board.util.StringUtils;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -9,12 +14,17 @@ import lombok.RequiredArgsConstructor;
 public class TagController {
   private final TagService tagService;
 
-  // TODO : TagController 작성 필요
+  // TODO : TagController �옉�꽦 �븘�슂
 
   /* Get */
 
   /* Post */
-
+  @PostMapping("tag/information")
+  public ResponseEntity<Long> addTagInformation(@RequestBody String information) {
+    information = StringUtils.removeSpecialCharacter(information);
+    Long result = tagService.addTag(information.trim());
+    return new ResponseEntity<> (result, HttpStatus.OK);
+  }
   /* Put */
 
   /* Delete */
