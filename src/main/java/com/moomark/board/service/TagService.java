a43f3d6a0518ddb59438ad8ee8f3ecab8ef159fa
Apisapple <38449976+Apisapple@users.noreply.github.com>
2021-11-14 04:21:08 +0900

    Update TagService.java
    불필요한 import 내용 제거

--- src/main/java/com/moomark/board/service/TagService.java
+++ src/main/java/com/moomark/board/service/TagService.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import com.moomark.board.domain.BoardTag;
 import com.moomark.board.domain.Tag;
 import com.moomark.board.domain.TagDto;
 import com.moomark.board.exception.ErrorCode;
