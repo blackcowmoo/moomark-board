b37490b57d74c96859fb72a60a1706bf57fd0da9
Apisapple <38449976+Apisapple@users.noreply.github.com>
2021-11-15 01:20:22 +0900

    Create TagServiceTest.java
    tagService 단위 테스트 추가

---
+++ src/test/java/com/moomark/board/service/TagServiceTest.java
@@ -0,0 +1,40 @@
+package com.moomark.board.service;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import com.moomark.board.domain.TagDto;
+import com.moomark.board.exception.JpaException;
+import com.moomark.board.repository.TagRepository;
+
+// @RestClientTest(TagService.class)
+
+@ExtendWith(MockitoExtension.class)
+class TagServiceTest {
+  @Mock
+  private TagRepository tagRepository;
+  @InjectMocks
+  private TagService tagService;
+
+  @Test
+  @DisplayName("tag찾기 기능이 잘 동작하는지 확인")
+  void findTagTest() throws JpaException {
+     TagDto tagDto = new TagDto(1L, "TEST");
+     when(tagRepository.save(any())).thenReturn(tagDto.toEntity());
+     tagService.saveTag(tagDto);
+     when(tagRepository.findById(1L)).thenReturn(Optional.of(tagDto.toEntity()));
+     
+     TagDto responseDto = tagService.findTagById(tagDto.getId());
+     
+     Assertions.assertThat(responseDto).isNotNull();
+     Assertions.assertThat(responseDto.getInformation()).isEqualTo(tagDto.getInformation());
+  }
+
+}
