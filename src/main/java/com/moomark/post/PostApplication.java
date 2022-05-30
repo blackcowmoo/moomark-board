533eab9bc35f83d9b3996eb37b789793c0d65907
micalgenus <micalgenus@gmail.com>
2022-05-31 00:49:49 +0900

    change package path

--- src/main/java/com/moomark/post/PostApplication.java
+++ src/main/java/com/moomark/post/PostApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MoomarkBoardApplication {
+public class PostApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MoomarkBoardApplication.class, args);
+		SpringApplication.run(PostApplication.class, args);
 	}
 
 }
