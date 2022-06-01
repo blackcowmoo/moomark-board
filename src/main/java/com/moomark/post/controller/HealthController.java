c9cd928ddd467cc7791576498e32a191156b61d0
micalgenus <micalgenus@gmail.com>
2022-06-01 23:01:17 +0900

    add health

---
+++ src/main/java/com/moomark/post/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.moomark.post.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class HealthController {
+  @GetMapping("/health")
+  public String getCategoryInfo() {
+    return "OK"
+  }
+}
