5716ab94ad2f547206961b67136fdb2378430c63
micalgenus <micalgenus@gmail.com>
2022-06-04 19:16:46 +0900

    remove read only

--- src/main/java/com/moomark/post/service/CommentService.java
+++ src/main/java/com/moomark/post/service/CommentService.java
@@ -22,7 +22,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class CommentService {
   private final PostRepository postRepository;
   private final CommentRepository commentRepository;
