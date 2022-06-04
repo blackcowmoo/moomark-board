e27d5b981e858e9d1d68794f28fb390ce89dbf74
GyeongSu Han <micalgenus@gmail.com>
2022-06-04 20:56:33 +0900

    Add write post (#12)
    * add write post
    
    * remove read only
    
    * remove read only
    
    * remove read only
    
    * remove read only
    
    * add test log
    
    * fix parameter
    
    * fix offset
    
    * add header
    
    * fix body

---
+++ src/test/java/com/moomark/post/controller/PostControllerTest.java
@@ -0,0 +1,47 @@
+package com.moomark.post.controller;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.moomark.post.domain.Post;
+
+import net.minidev.json.JSONObject;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PostControllerTest {
+  @Autowired
+  private MockMvc mvc;
+
+  @Autowired
+  private ObjectMapper mapper;
+
+  @Test
+  public void writePost() throws Exception {
+    String testTitle = "testTitle";
+    String testContent = "testContent";
+
+    JSONObject requestParams = new JSONObject();
+    requestParams.put("title", testTitle);
+    requestParams.put("content", testContent);
+
+    Post post = mapper.readValue(mvc
+        .perform(post("/api/v1/post")
+            .header("Content-Type", "application/json").header("x-moom-user-id", "TEST@test")
+            .content(requestParams.toJSONString()))
+        .andExpect(status().isOk()).andReturn().getResponse().getContentAsString(), Post.class);
+
+    assertEquals(post.getTitle(), testTitle);
+    assertEquals(post.getContent(), testContent);
+    assertEquals(post.getRecommendCount(), 0);
+    assertEquals(post.getViewsCount(), 0);
+  }
+}
