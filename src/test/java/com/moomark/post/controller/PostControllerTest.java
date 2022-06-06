5e1454f08636618a7f9324ff14e461859fbd553a
micalgenus <micalgenus@gmail.com>
2022-06-06 19:38:26 +0900

    typo

--- src/test/java/com/moomark/post/controller/PostControllerTest.java
+++ src/test/java/com/moomark/post/controller/PostControllerTest.java
@@ -1,10 +1,7 @@
 package com.moomark.post.controller;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
