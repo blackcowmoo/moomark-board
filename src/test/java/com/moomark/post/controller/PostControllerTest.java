d5fc2d7da0afcb051901fac2a16a33a7a944bdde
micalgenus <micalgenus@gmail.com>
2022-07-03 17:33:21 +0900

    add passport test

--- src/test/java/com/moomark/post/controller/PostControllerTest.java
+++ src/test/java/com/moomark/post/controller/PostControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.moomark.post.PassportUtil;
 import com.moomark.post.model.entity.Post;
 
 import net.minidev.json.JSONObject;
@@ -28,6 +29,9 @@ public class PostControllerTest {
 
   @Autowired
   private ObjectMapper mapper;
+
+  @Autowired
+  private PassportUtil passportUtil;
 
   @Test
   @Order(1)
@@ -50,7 +54,7 @@ public class PostControllerTest {
 
     Post post = mapper.readValue(mvc
         .perform(post("/api/v1/post")
-            .header("Content-Type", "application/json").header("x-moom-user-id", "TEST@test")
+            .header("Content-Type", "application/json").header("x-moom-passport", passportUtil.generatePassport())
             .content(requestParams.toJSONString()))
         .andExpect(status().isOk()).andReturn().getResponse().getContentAsString(), Post.class);
 
@@ -109,7 +113,7 @@ public class PostControllerTest {
     assertEquals(resultPost.getTitle(), testTitle);
     assertEquals(resultPost.getContent(), testContent);
   }
-  
+
   @Test
   @Order(6)
   public void writePostEmptyTitle() throws Exception {
