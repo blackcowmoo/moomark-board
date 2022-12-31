969b4b1778e594d043db1b1b20333589f58972f2
Apisapple <38449976+Apisapple@users.noreply.github.com>
2022-12-31 21:26:03 +0900

    Add ProfileCondition
    ProfileCondition class 추가

---
+++ src/main/java/com/moomark/post/configuration/profile/LocalProfileCondition.java
@@ -0,0 +1,11 @@
+package com.moomark.post.configuration.profile;
+
+import java.util.List;
+
+public class LocalProfileCondition extends BaseProfileCondition{
+
+  @Override
+  List<Profile> getTargetProfile() {
+    return List.of(Profile.LOCAL);
+  }
+}
