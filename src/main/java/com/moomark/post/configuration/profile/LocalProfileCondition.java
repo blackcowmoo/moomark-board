0bafee5a6d38b0f8a840ad8b8dd8bfa0c68a94df
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-01-10 23:20:29 +0900

    Update LocalProfileCondition.java
    [DOC] Checkstyle에 맞게 수정

--- src/main/java/com/moomark/post/configuration/profile/LocalProfileCondition.java
+++ src/main/java/com/moomark/post/configuration/profile/LocalProfileCondition.java
@@ -2,7 +2,7 @@ package com.moomark.post.configuration.profile;
 
 import java.util.List;
 
-public class LocalProfileCondition extends BaseProfileCondition{
+public class LocalProfileCondition extends BaseProfileCondition {
 
   @Override
   List<Profile> getTargetProfile() {
