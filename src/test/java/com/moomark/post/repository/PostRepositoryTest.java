7d68743d5ca9fd1ebbe12a320922fddbbe5d80c3
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-01-12 02:49:16 +0900

    Update PostRepositoryTest.java
    Add test init

--- src/test/java/com/moomark/post/repository/PostRepositoryTest.java
+++ src/test/java/com/moomark/post/repository/PostRepositoryTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import com.moomark.post.model.entity.Post;
 import java.util.List;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,11 @@ class PostRepositoryTest {
 
   @Autowired
   private PostRepository postRepository;
+
+  @BeforeEach
+  void initTest() {
+    postRepository.deleteAll();
+  }
 
   @Test
   void findByTitle() {
