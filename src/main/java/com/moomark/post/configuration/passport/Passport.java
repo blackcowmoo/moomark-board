a77bf2be7bf136ac83284517274e861bb54f8200
micalgenus <micalgenus@gmail.com>
2023-01-24 00:58:04 +0900

    fix checkstyle

--- src/main/java/com/moomark/post/configuration/passport/Passport.java
+++ src/main/java/com/moomark/post/configuration/passport/Passport.java
@@ -6,7 +6,7 @@ import java.sql.Timestamp;
 
 @Data
 public class Passport {
-    public Timestamp exp; // expired timestamp
-    public String key;
-    public String hash;
+  public Timestamp exp; // expired timestamp
+  public String key;
+  public String hash;
 }
