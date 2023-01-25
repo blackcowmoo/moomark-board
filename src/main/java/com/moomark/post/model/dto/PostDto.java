d3f48766bb6664fa9309e90c107cff4c45a3459d
Apisapple <38449976+Apisapple@users.noreply.github.com>
2023-01-26 02:43:45 +0900

    Update PostDto.java
    Add toDto function

--- src/main/java/com/moomark/post/model/dto/PostDto.java
+++ src/main/java/com/moomark/post/model/dto/PostDto.java
@@ -1,6 +1,9 @@
 package com.moomark.post.model.dto;
 
+import com.moomark.post.model.entity.Post;
+import com.moomark.post.model.entity.PostCategory;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import lombok.AccessLevel;
@@ -29,4 +32,27 @@ public class PostDto {
   private LocalDateTime uploadTime;
   
   private List<CategoryDto> categories;
+
+  public static PostDto toDto(Post post) {
+    List<CategoryDto> categoryDtos = new ArrayList<>();
+    for (PostCategory category : post.getPostCategory()) {
+      categoryDtos.add(CategoryDto.builder()
+          .id(category.getId())
+          .categoryType(category.getCategory().getCategoryType())
+          .parentsId(category.getCategory().getParent().getId())
+          .build()
+      );
+    }
+
+    return PostDto.builder()
+        .id(post.getId())
+        .userId(post.getUserId())
+        .recommendCount(post.getRecommendCount())
+        .viewsCount(post.getViewsCount())
+        .title(post.getTitle())
+        .content(post.getContent())
+        .uploadTime(post.getUploadTime())
+        .categories(categoryDtos)
+        .build();
+  }
 }
